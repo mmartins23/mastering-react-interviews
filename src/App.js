3de6e0cd338c1ff805ce54b58f234e3e41245537
Marlon Martins <marlon-martins@outlook.com>
2024-06-11 09:46:29 +0200

    3. Component Based Architecture [FIX]

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { PersonDetails } from './components/PersonDetails';
 function App() {
   return (
     <div className="App">
+      <h2>3. Component Based Architecture</h2>
       <PersonDetails name="Anne" age={20} />
       <PersonDetails name="Bob" age={22} />
       <PersonDetails name="Charles" age={24} />
+      <hr />
     </div>
   );
 }
