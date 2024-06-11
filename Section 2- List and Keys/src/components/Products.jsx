b1cce4c2c8e25ae5f6a4734274039ad88359fdaf
Marlon Martins <marlon-martins@outlook.com>
2024-06-11 22:26:56 +0200

    29. Keys

--- Section 2- List and Keys/src/components/Products.jsx
+++ Section 2- List and Keys/src/components/Products.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 
 const Products = () => {
 
-    const fruits = ["Apple", "Banana", "Cocunut", "Mango"];
+    const fruits = [
+        { id: 1, name: "Apple" },
+        { id: 2, name: "Banana" },
+        { id: 3, name: "Coconut" },
+        { id: 4, name: "Mango" }
+    ]
     const [products] = useState(fruits);
 
     return (
         <div>
-            {products.map(product => (
-                <li>{product}</li>
+            {products.map((product) => (
+                <li key={product.id}>{product.name}</li>
             ))}
         </div>
     )
