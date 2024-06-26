92dac5c7afa0013e722d4674bd95ac5cdbb177f0
Marlon Martins <marlon-martins@outlook.com>
2024-06-26 12:43:31 +0200

    88. Setting Up Redux App

--- Section 9- Redux/redux-demo/src/App.js
+++ Section 9- Redux/redux-demo/src/App.js
@@ -1,6 +1,6 @@
-import Cart from '../components/Cart';
-import Purchase from '../components/Purchase';
-import Total from '../components/Total';
+import Cart from './components/Cart';
+import Purchase from './components/Purchase';
+import Total from './components/Total';
 import './App.css';
 
 function App() {
